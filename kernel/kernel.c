9d5e0abff6944de5af8839c218540c7b3171dc62
HSNHK <hsnmmdim@gmail.com>
2021-10-15 21:41:12 +0330

    kernel entry

---
+++ kernel/kernel.c
@@ -0,0 +1,13 @@
+#include "../drivers/keyboard/keyboard.h"
+#include "../drivers/vga/console.h"
+#include "values.h"
+#include "utils.h"
+#include "types.h"
+
+void entry(){
+    init_vga(WHITE, BLACK);
+    
+    pr_info("hello");
+}
+
+
